2d69b585d4c21bf773e985d302ca724ea71e035e
mattiasmucherie <mattias.mattias.m@gmail.com>
2019-04-03 00:39:28 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,35 @@
     <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
+    <v-footer dark height="auto">
+      <v-card class="flex" flat tile>
+        <v-card-title>
+          <span class="subheading">
+            Created by
+            <strong>Mattias Mucherie</strong>
+          </span>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            class="mx-3"
+            dark
+            icon
+            :href="link.link"
+            target="_blank"
+          >
+            <v-icon size="24px">{{ link.icon }}</v-icon>
+          </v-btn>
+        </v-card-title>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
+         
 
+          
 <script>
 import Navbar from "@/components/Navbar";
 export default {
@@ -14,6 +40,17 @@ export default {
   components: { Navbar },
   data() {
     return {
+      links: [
+        {
+          icon: "fas fa-sitemap",
+          link: "https://mattiasmucherie.github.io/"
+        },
+        { icon: "fab fa-github", link: "https://github.com/mattiasmucherie" },
+        {
+          icon: "fab fa-linkedin",
+          link: "https://www.linkedin.com/in/mattias-mucherie/"
+        }
+      ]
       //
     };
   }
